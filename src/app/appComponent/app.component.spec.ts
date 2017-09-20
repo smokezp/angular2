64dc067f95031f8d547b0849cd0f09fb69800b03
smokezp <oleksii.kutyriev@jsninjas.awsapps.com>
2017-09-20 12:47:00 +0300

    fix tests

--- src/app/appComponent/app.component.spec.ts
+++ src/app/appComponent/app.component.spec.ts
@@ -1,7 +1,5 @@
 import {TestBed, ComponentFixture} from '@angular/core/testing';
 import {AppComponent} from './app.component';
-import {By}              from '@angular/platform-browser';
-import {DebugElement}    from '@angular/core';
 import {AppRoutingModule}     from '../app-routing.module';
 import {DashboardComponent}   from '../dashboardComponent/dashboard.component';
 import {HeroDetailComponent}  from '../heroDetailComponent/hero-detail.component';
@@ -12,8 +10,6 @@ import {APP_BASE_HREF} from '@angular/common';
 
 let fixture: ComponentFixture<AppComponent>;
 let comp: AppComponent;
-let de: DebugElement;
-let el: HTMLElement;
 
 describe('AppComponent', () => {
   beforeEach(() => {
@@ -31,31 +27,25 @@ describe('AppComponent', () => {
 
     fixture = TestBed.createComponent(AppComponent);
     comp = fixture.componentInstance;
-    de = fixture.debugElement.query(By.css('h1'));
-    el = de.nativeElement;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('Tour of Heroes');
   });
 
   it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Tour of Heroes');
   });
 
   it('should render 2 button-links', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     const aSelect = compiled.querySelectorAll('a');
--- src/app/dashboardComponent/dashboard.component.spec.ts
+++ src/app/dashboardComponent/dashboard.component.spec.ts
@@ -1,6 +1,4 @@
 import {TestBed, ComponentFixture} from '@angular/core/testing';
-// import {AppComponent} from './app.component';
-import {By}              from '@angular/platform-browser';
 import {DebugElement}    from '@angular/core';
 import {AppRoutingModule}     from '../app-routing.module';
 import {DashboardComponent}   from './dashboard.component';
@@ -8,17 +6,14 @@ import {HeroDetailComponent}  from '../heroDetailComponent/hero-detail.component
 import {HeroesComponent}      from '../heroesComponent/heroes.component';
 import {HeroSearchComponent}         from '../heroSeachComponent/hero-search.component';
 import {HeroService}         from '../hero/hero.service';
-import {Http}       from '@angular/http';
 import {HttpModule}    from '@angular/http';
 import {FormsModule} from '@angular/forms';
 import {APP_BASE_HREF} from '@angular/common';
 
 let fixture: ComponentFixture<DashboardComponent>;
 let comp: DashboardComponent;
-let de: DebugElement;
-let el: HTMLElement;
 
-fdescribe('DashboardComponent', () => {
+describe('DashboardComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [AppRoutingModule, FormsModule, HttpModule],
@@ -32,40 +27,9 @@ fdescribe('DashboardComponent', () => {
     });
 
     fixture = TestBed.createComponent(DashboardComponent);
-    comp = fixture.componentInstance;
-    console.log(fixture);
-    de = fixture.debugElement.query(By.css('h3'));
-    el = de.nativeElement;
   });
 
   it('should create the app', () => {
     expect(true).toBe(true);
   });
-  // it('should create the app', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.debugElement.componentInstance;
-  //   expect(app).toBeTruthy();
-  // });
-  //
-  // it(`should have as title 'app'`, () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.debugElement.componentInstance;
-  //   expect(app.title).toEqual('Tour of Heroes');
-  // });
-  //
-  // it('should render title in a h1 tag', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.debugElement.nativeElement;
-  //   expect(compiled.querySelector('h1').textContent).toContain('Tour of Heroes');
-  // });
-  //
-  // it('should render 2 button-links', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.debugElement.nativeElement;
-  //   const aSelect = compiled.querySelectorAll('a');
-  //   expect(aSelect[0].textContent).toContain('Dashboard');
-  //   expect(aSelect[1].textContent).toContain('Heroes');
-  // });
 });
