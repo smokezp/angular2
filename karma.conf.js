0b0ffd9b36b2acbe3a1ba3f311ca0fde8b912fdc
smokezp <smoke111@email.ru>
2018-02-03 19:05:16 +0200

    sss

--- karma.conf.js
+++ karma.conf.js
@@ -37,7 +37,6 @@ module.exports = function (config) {
       }
     },
     phantomjsLauncher: {
-      // Have phantomjs exit if a ResourceError is encountered (useful if karma exits without killing phantom)
       exitOnResourceError: true
     },
     reporters: ['progress', 'kjhtml'],
